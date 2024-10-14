31c8328226548dc275c30e5aae956bbd1ed572e7
erwan sorroche <erwan.sorroche@epfedu.fr>
2024-10-14 09:44:37 +0200

    insertion image gif

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core"
 import { Link } from "models/links.model"
+import { Router } from '@angular/router';
 
 @Component({
   selector: "epf-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"],
 })
+
 export class NavbarComponent {
   links: Link[] = []
 
